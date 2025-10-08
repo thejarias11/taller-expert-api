93eac0f04acdd16312e49751add0532ea2e13b54
Thejarias <theariasduran@gmail.com>
2025-10-08 06:01:58 -0500

    feat: add package.json and server.js for production

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-// Importar la aplicación principal
+// Importar dependencias
 const app = require('./src/app');
 
-// Configurar el puerto (usará el del entorno o 3000 por defecto)
+// Configurar puerto
 const PORT = process.env.PORT || 3000;
 
-// Iniciar el servidor
+// Iniciar servidor
 app.listen(PORT, () => {
     console.log(`🚀 Servidor ejecutándose en http://localhost:${PORT}`);
 });
